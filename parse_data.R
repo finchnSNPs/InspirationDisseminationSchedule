0c31b24c2aa05923487ffb0410c2a5d09756f742
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-09-09 20:33:21 -0700

    check if file exists before writing.

--- parse_data.R
+++ parse_data.R
@@ -162,7 +162,10 @@ write.table(x = outmat, file = "availability.csv", sep = ",", col.names = NA)
 
 # Writing the dossiers in markdown
 for (i in names(avail_list)){
-  make_dossier(i, IDS, avail_list)
+  fname <- make_filename(i, wd = ".", newdir = "/dossiers/md_files")
+  if (!file.exists(fname)){
+    make_dossier(i, IDS, avail_list, wd = ".")
+  }
 }
 
 # processing the dossiers with pandoc
