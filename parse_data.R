fd4394f4eed3b852e15e4ce378e96c974c93e170
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-09-09 21:16:10 -0700

    fixed mistake where data wasn't being written to files

--- parse_data.R
+++ parse_data.R
@@ -162,7 +162,7 @@ write.table(x = outmat, file = "availability.csv", sep = ",", col.names = NA)
 
 # Writing the dossiers in markdown
 for (i in names(avail_list)){
-  fname <- make_filename(i, wd = ".", newdir = "/dossiers/md_files")
+  fname <- make_filename(i, wd = ".", newdir = "/dossiers/md_files/")
   if (!file.exists(fname)){
     make_dossier(i, IDS, avail_list, wd = ".")
   }
